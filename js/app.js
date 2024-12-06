6de421f514ccef02d2b976f9b62107757c5afcaa
jrdurrcodes <biycoachn@yahoo.com>
2024-12-06 15:08:18 -0600

    made changes in javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-const
+const switchButtons = document.getElementById('switch-button')
+const body = document.getElementById('body')[0]
+    
+    const styles = {
+        isOn: {
+            text: 'on'
+        },
+        isOff: {
+            text: 'off'
+        }
+    }
+
+        
+
+
+
+
+
+
